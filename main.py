f34843cc59724f10c3bc1e8430a7b36d6020fd82
JamesFisken <99928794+JamesFisken@users.noreply.github.com>
2022-05-01 21:42:22 +1200

    finished the necessary steps for computing the first hidden layer's neurons

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import math
 import numpy as np
 import random
 from PIL import Image
+import time
+start_time = time.time()
+
+np.set_printoptions(suppress=True)
 
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
@@ -38,13 +42,12 @@ hiddenlayer_size = 20 #size of a hidden layer
 hiddenlayers = 5 #amount of hidden layers
 
 
-inputs = [] #holds all iterations of the inputnode class and with that their weights
-hiddenlayer = []
-outputlayer = []
+
 
 class inputnode:
   def __init__(self, value):
     self.weight = [round(random.uniform(-1.0, 1.0), 2) for i in range(hiddenlayer_size)] #sets a random weight(-1.00, 1.00) for all the synapses spreading out from the input node
+
     self.value = value
 
     self.type = "inputnode"
@@ -56,11 +59,13 @@ class hiddenlayernode:
     self.position = position #is set to the position of a neuron in a dedicated layer
 
     self.weight = [round(random.uniform(-1.0, 1.0), 2) for i in range(hiddenlayer_size)]
-    self.bias = 0  # this will be made random
+    self.bias = round(random.uniform(-1.0, 1.0), 2)
 
     self.value = 0 #math will determine this
 
     self.type = "hiddenlayernode"
+
+
 class outputnode:
   def __init__(self, number):
     self.number = number
@@ -69,6 +74,12 @@ class outputnode:
     self.type = "outputnode"
 
 def init():
+  global inputs, hiddenlayer, outputlayer
+  inputs = []  # holds all iterations of the inputnode class and with that their weights
+  hiddenlayer = [] #holds all iterations of the hiddenlayernode class
+  outputlayer = [] #holds all iterations of the outputnode class
+
+
   for i in inputs_array: inputs.append(inputnode(i)) #creates all input neurons as an instance of the inputnode class and adds them to a list
 
   for x in range(hiddenlayers):
@@ -77,20 +88,56 @@ def init():
 
   for i in range(10): #getting all digits 1-10
     outputlayer.append(outputnode(i))
+
+def neuralnetwork(inputlayer, hiddenlayer, outputlayer):
+  layer = 0
+
+
+  weights_temp = []
+  final_values = []
+  bias = []
+  for x in range(len(inputlayer)):
+    for y in range(len(inputlayer[x].weight)):
+      weights_temp.append(inputlayer[x].weight[y])
+  for node in hiddenlayer:
+    if node.layer == layer:
+      bias.append(node.bias)
+
+  inputs = np.array([inputlayer[i].value for i in range(len(inputlayer))])
+  weights = np.array(weights_temp)
+  bias = np.array(bias)
+
+  bias = np.reshape(bias, (20, 1)) #reshapes everything so that its ready for matrix multiplication
+  weights = np.reshape(weights, (20, 1024))
+  inputs = np.reshape(inputs, (1024, 1))
+
+
+  values = np.matmul(weights, inputs) #columns(1) must equal rows(2)
+  values = np.add(values, bias)
+  for x in values:
+    final_values.append(sigmoid(x))
+
+
+  print(final_values) #final values should be given to the hidden layer nodes
+
+
+
 init()
+neuralnetwork(inputs, hiddenlayer, outputlayer)
+
 
 
 #---- bug testing ----
-for i in range(len(inputs)):
-  print(inputs[i].type, i, inputs[i].weight) #prints all inputs and the weights of their synapses
+#for i in range(len(inputs)):
+  #print(inputs[i].type, i, inputs[i].weight) #prints all inputs and the weights of their synapses
 
-for i in range(len(hiddenlayer)):
-  print(hiddenlayer[i].type, "layer: ", hiddenlayer[i].layer, "position: ", hiddenlayer[i].position, hiddenlayer[i].weight) #prints all hiddenlayer nodes and their relevant infomation
+#for i in range(len(hiddenlayer)):
+  #print(hiddenlayer[i].type, "layer: ", hiddenlayer[i].layer, "position: ", hiddenlayer[i].position, hiddenlayer[i].weight) #prints all hiddenlayer nodes and their relevant infomation
 
-for i in range(len(outputlayer)):
-  print(outputlayer[i].type, outputlayer[i].number, outputlayer[i].value) #prints all outputs and their values
-
-
+#for i in range(len(outputlayer)):
+  #print(outputlayer[i].type, outputlayer[i].number, outputlayer[i].value) #prints all outputs and their values
 
 
+
+print("code took: ", time.time() - start_time, "seconds to run")
 
